aff1b946250e49f42159cbe4695add53250f8171
Wir3s <113479857+Wir3s@users.noreply.github.com>
2023-01-18 17:49:23 -0600

    Setting up main menu

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 const inquirer = require("inquirer");
+
+const mainMenuQ = [
+  {
+    type: "list",
+    name: "mmPrompt",
+    choices: [
+      "View All Employees",
+      "Add Employee",
+      "Update Employee Role",
+      "View All Roles",
+      "Add Role",
+      "View All Departments",
+      "Add Department",
+      "Quit",
+    ],
+    message: "What would you like to do?",
+  },
+];
+
+function mainMenu() {
+  inquirer.createPromptModule(mainMenuQ).then((data) => {
+    switch (data.mmPrompt) {
+      case "View All Employees":
+        viewAllEmp();
+        break;
+      case "Add Employee":
+        break;
+      case "Update Employee Role":
+        break;
+      case "View All Roles":
+        break;
+      case "Add Role":
+        addRole();
+        break;
+      case "View All Departments":
+        viewAllDept();
+        break;
+      case "Add Department":
+        addDept();
+        break;
+      case "Quit":
+        break;
+    }
+  });
+}
