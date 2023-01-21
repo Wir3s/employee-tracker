3b44e2e021511df97c29f4ec8cf77680adb4acbc
Wir3s <113479857+Wir3s@users.noreply.github.com>
2023-01-21 16:14:52 -0600

    Three queries working

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,19 +4,21 @@ VALUES (100, "Sales"),
        (300, "IT");
 
 INSERT INTO role (id, title, salary, department_id)
-VALUES (301, "Programmer", 80.00, 300),       
-       (302, "Testing", 60.00, 300),
-       (101, "Sales Rep", 70.00, 100),
-       (102, "Junior Sales Rep", 30.00, 100),
-       (202, "Floor Cleaner", 40.00, 200),
-       (203, "Window Washer", 50.00, 200),
-       (201, "Cleaning Leader", 80.00, 200);
+VALUES (301, "Programmer", 80000, 300),       
+       (302, "Testing", 60000, 300),
+       (101, "Sales Rep", 70000, 100),
+       (102, "Junior Sales Rep", 30000, 100),
+       (202, "Floor Cleaner", 40000, 200),
+       (203, "Window Washer", 50000, 200),
+       (201, "Cleaning Leader", 80000, 200);
+    
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (001, "Violet", "Bass", , 301, NULL),
+VALUES (001, "Violet", "Bass", 301, NULL),
        (002, "Calvin", "Menace", 302, 001),
        (003, "Beatrice", "Worg", 101, NULL),
        (004, "Hubert", "Hool", 202, 006),
        (005, "Yhorm", "Giant", 203, 006),
        (006, "Anais", "Deflew", 201, NULL),
        (007, "Runky", "Reeves", 102, 003);
+
--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 const inquirer = require("inquirer");
+// Import and require mysql2
+const mysql = require("mysql2");
+
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    // MySQL username,
+    user: "root",
+    // MySQL password
+    password: "turnip",
+    database: "employees_db",
+  },
+  console.log(`Connected to the classlist_db database.`)
+);
 
 const mainMenuQ = [
   {
@@ -18,6 +32,14 @@ const mainMenuQ = [
   },
 ];
 
+const newDeptQ = [
+  {
+    type: "input",
+    name: "newD",
+    message: "What is name of the new Department?",
+  },
+];
+
 function mainMenu() {
   inquirer.prompt(mainMenuQ).then((data) => {
     switch (data.mmPrompt) {
@@ -27,8 +49,10 @@ function mainMenu() {
       case "Add Employee":
         break;
       case "Update Employee Role":
+        updateEmpR();
         break;
       case "View All Roles":
+        viewAllRoles();
         break;
       case "Add Role":
         addRole();
@@ -45,11 +69,90 @@ function mainMenu() {
   });
 }
 
-// Notes from Sara:
-// mainMenu();
+const selEmpFN = "SELECT first_name FROM employee";
+const selAllEmp = "SELECT * from role";
 
+const updateEmpRoleQ = [
+  {
+    type: "list",
+    name: "empL",
+    message: "Which Employee?",
+    choices: ["1", "2"],
+  },
+];
+
+// function viewEmpChoices() {
+//   db.query(
+//     {
+//       sql: "SELECT * FROM role JOIN department ON role.department_id = department.id",
+//       rowsAsArray: true,
+//     },
+//     function (err, results, fields) {
+//       {
+//         console.table(results);
+//         console.table(fields);
+//         mainMenu();
+//       }
+//     }
+//   );
+// }
+
+function viewAllEmp() {
+  db.query(selEmpFN, function (err, results) {
+    console.table(results);
+    mainMenu();
+  });
+}
+
+function viewAllDept() {
+  db.query("SELECT * FROM department", function (err, results) {
+    console.table(results);
+    mainMenu();
+  });
+}
+
+function viewAllRoles() {
+  db.query(
+    "SELECT * FROM role JOIN department ON role.department_id = department.id",
+    function (err, results) {
+      console.table(results, ["id", "title", "salary", "name"]);
+      mainMenu();
+    }
+  );
+}
+
+function addDept() {
+  inquirer.prompt(newDeptQ).then((data) => {
+    db.query(
+      "INSERT INTO department (name) VALUES (?)",
+      [data.newD],
+      function (err, results) {
+        console.log(results);
+        viewAllDept();
+      }
+    );
+  });
+}
+// Update Employee Roles - Attempt 1
+function updateEmpR() {
+  inquirer.prompt(updateEmpRoleQ).then((data) => {
+    console.log(data);
+  });
+}
+
+// Update Employee Roles - Attempt 2
+// {
+//     db.promise().query("Select * from employee")
+//         .then((rows) => {
+//             console.log(rows);
+//         })
+//         .catch(console.log)
+//         .then( () => db.end())
+// }
+
+mainMenu();
+
+// Notes from Sara:
 // addEmployee(employee) {
 
 //   return this.connection.promise().query("INSERT INTO employee SET ?", employee);
-
-// }
